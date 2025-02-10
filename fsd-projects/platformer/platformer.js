d6a5dfc41293938c6b34224e5803c66726af6c24
ELAN1of1 <landry975@wearescpps.org>
2025-02-10 14:33:22 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -21,28 +21,37 @@ $(function () {
     createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
+    createPlatform(-50, -50, 50, canvas.height + 500); // right wall
 
     //////////////////////////////////
     // ONLY CHANGE BELOW THIS POINT //
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    // ();
 
 
     // TODO 2 - Create Platforms
-
-
-
+    createPlatform(500, 0, 20, 290);
+createPlatform(50, 200, 60, 30, "orange");
+createPlatform(300, 500, 200, 10);
+createPlatform(900, 570, 150, 4000);
+createPlatform(600, 630, 200, 10);
+createPlatform(250, 650, 200, 20);
+createPlatform(1100, 640, 120, 50);
+createPlatform(600, 400, 160, 30);
 
     // TODO 3 - Create Collectables
-
-
-
+    createCollectable("steve", 450, 20, 0.7, 0.1);
+    createCollectable("diamond", 800, 170, 0.5, 0.7);
+    createCollectable("diamond", 1000, 70, 0.5, 0.7);
+    createCollectable("database", 1250, 76, 0.5, 0.5);
+    createCollectable("database", 150, 76, 0.5, 0.5);
     
     // TODO 4 - Create Cannons
-
-
+    createCannon("top", 300, 1500);
+    createCannon("right", 250, 2000);
+    createCannon("left", 400, 2100);
     
     
     //////////////////////////////////
